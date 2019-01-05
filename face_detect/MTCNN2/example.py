63ac707e9b4328d482690d168d0c25567bba48d0
ch-tseng <ch.tseng@sunplusit.com>
2019-01-05 09:33:22 +0800

    update

---
+++ face_detect/MTCNN2/example.py
@@ -0,0 +1,28 @@
+#pip3 install mtcnn
+
+import cv2
+from mtcnn.mtcnn import MTCNN
+detector = MTCNN()
+
+def getFaces(img):
+    faces = detector.detect_faces(img)
+    bboxes = []
+    for face in faces:
+        x = face["box"][0]
+        y = face["box"][1]
+        w = face["box"][2]
+        h = face["box"][3]
+
+        bboxes.append((x,y,w,h))
+
+    return bboxes
+
+pic = cv2.imread("peoples2.jpg")
+faces = getFaces(pic)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle( pic,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("TEST", pic)
+cv2.waitKey(0)
+
