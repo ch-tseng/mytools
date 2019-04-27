8490e8e2e55b273eec2ee3ee1f144ea9da3be125
Tseng Cheng Hsun <chtseng@Tsengde-iMac.local>
2019-04-27 21:35:29 +0800

    update

---
+++ functions/fps_count.py
@@ -0,0 +1,9 @@
+#fps count
+start = time.time()
+def fps_count(num_frames):
+    end = time.time()
+    seconds = end - start
+    fps  = num_frames / seconds;
+    print("Estimated frames per second : {0}".format(fps))
+    return fps
+
