7f2a8087fdff1b788a6eaa2f57867b81364ace6b
Tseng Cheng Hsun <ch.tseng@sunplusit.com>
2019-05-15 00:01:48 +0800

    Create rpi_h264_support.sh

---
+++ install_scripts/rpi_h264_support.sh
@@ -0,0 +1,12 @@
+cd ~
+git clone --depth 1 http://git.videolan.org/git/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make -j4
+sudo make install
+cd ~
+git clone git://source.ffmpeg.org/ffmpeg --depth=1
+cd ffmpeg
+./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
+make -j4
+sudo make install
