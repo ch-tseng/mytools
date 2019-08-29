0a80446c5eb2be278f57c6d4f17ba242706d1827
ch-tseng <ch.tseng@sunplusit.com>
2019-08-29 18:22:01 +0800

    update

--- yolo/grep_objects_to_images.py
+++ yolo/grep_objects_to_images.py
@@ -10,12 +10,13 @@ yolo = opencvYOLO(modeltype="yolov3", \
     cfg="../../../darknet/cfg/yolov3.cfg")
 
 objects = ("truck", "car", "motorcycle", "bus", "bicycle")
-output_path = "/home/chtseng/output/yolo_objects"
+output_path = "/media/flash/cars_waiting/yolo_objects"
+frame_interval =30
 
 inputType = "video"  # webcam, image, video
 media = "/home/chtseng/works/alpr/videos/4K/IMG_2195.MOV"
-write_video = False
-video_out = "/media/sf_VMshare/out_hand1_yolov3.avi"
+write_video = True
+video_out = "/media/flash/cars_waiting/IMG_2195.avi"
 output_rotate = False
 rotate = 0
 
@@ -64,58 +65,60 @@ if __name__ == "__main__":
 
             if(write_video is True):
                 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-                out = cv2.VideoWriter(video_out,fourcc, 30.0, (int(width),int(height)))
+                out = cv2.VideoWriter(video_out,fourcc, int(30.0/frame_interval), (int(width),int(height)))
 
         frameID = 0
         obj_count = {}
 
         while True:
-            hasFrame, frame = INPUT.read()
-            frame_org = frame.copy()
-            # Stop the program if reached end of video
-            if not hasFrame:
-                print("Done processing !!!")
-                break
+            if(frameID % frame_interval == 0):
+                hasFrame, frame = INPUT.read()
+                frame_org = frame.copy()
+                # Stop the program if reached end of video
+                if not hasFrame:
+                    print("Done processing !!!")
+                    break
 
-            if(output_rotate is True):
-                frame = imutils.rotate(frame, rotate)
+                if(output_rotate is True):
+                    frame = imutils.rotate(frame, rotate)
 
-            yolo.getObject(frame, labelWant=objects, drawBox=True, bold=2, textsize=1.2, bcolor=(0,255,0), tcolor=(0,0,255))
-            print ("Object counts:", yolo.objCounts)
-            #yolo.listLabels()
+                yolo.getObject(frame, labelWant=objects, drawBox=True, bold=2, textsize=1.2, bcolor=(0,255,0), tcolor=(0,0,255))
+                print ("Object counts:", yolo.objCounts)
+                #yolo.listLabels()
 
-            for i, label in enumerate(yolo.labelNames):
-                folder_path = os.path.join(output_path, label)
-                chk_path(folder_path)
-                box = yolo.bbox[i]
-                print(box)
-                x, y, w, h = box[0], box[1], box[2], box[3]
-                img_area = frame_org[y:y+h, x:x+w]
+                for i, label in enumerate(yolo.labelNames):
+                    folder_path = os.path.join(output_path, label)
+                    chk_path(folder_path)
+                    box = yolo.bbox[i]
+                    x, y, w, h = box[0], box[1], box[2], box[3]
+                    img_area = frame_org[y:y+h, x:x+w]
 
-                if(label in obj_count):
-                    counts = obj_count[label]+1
-                else:
-                    counts = 1
+                    if(label in obj_count):
+                        counts = obj_count[label]+1
+                    else:
+                        counts = 1
 
-                obj_count.update({label:counts})
+                    obj_count.update({label:counts})
 
-                filename = label + '_' + str(counts) + '.jpg'
-                print("save object file:", filename)
-                cv2.imwrite(os.path.join(folder_path, filename), img_area)
+                    filename = label + '_' + str(counts) + '.jpg'
+                    print("save object file:", filename)
+                    cv2.imwrite(os.path.join(folder_path, filename), img_area)
 
-            #print("classIds:{}, confidences:{}, labelName:{}, bbox:{}".\
-            #    format(len(yolo.classIds), len(yolo.scores), len(yolo.labelNames), len(yolo.bbox)) )
+                #print("classIds:{}, confidences:{}, labelName:{}, bbox:{}".\
+                #    format(len(yolo.classIds), len(yolo.scores), len(yolo.labelNames), len(yolo.bbox)) )
 
-            #cv2.imshow("Frame", imutils.resize(frame, width=850))
-            frameID += 1
-            fps_count(frameID)
+                #cv2.imshow("Frame", imutils.resize(frame, width=850))
+                frameID += 1
+                #fps_count(frameID)
 
-            if(write_video is True):
-                out.write(frame)
-
-            k = cv2.waitKey(1)
-            if k == 0xFF & ord("q"):
                 if(write_video is True):
-                    out.release()
+                    out.write(frame)
 
-                break
+                k = cv2.waitKey(1)
+                if k == 0xFF & ord("q"):
+                    if(write_video is True):
+                        out.release()
+
+                    break
+
+            frameID += 1
