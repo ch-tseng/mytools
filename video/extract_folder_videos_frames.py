ca98b06a32762ff06e2d331a83df781d247a2e6b
Tseng Cheng Hsun <ch.tseng@sunplusit.com>
2019-12-28 01:18:41 +0800

    update

--- video/extract_folder_videos_frames.py
+++ video/extract_folder_videos_frames.py
@@ -58,7 +58,7 @@ for vfile in os.listdir(videoFolder):
                     if(resizeWidth>0):
                         img = imutils.resize(img, width=resizeWidth)
 
-                    if not os.path.exists(extract_folder):
+                    if not os.path.exists(os.path.join(extract_folder,filename)):
                         print("make folder:", extract_folder)
                         os.makedirs(os.path.join(extract_folder,filename))
 
