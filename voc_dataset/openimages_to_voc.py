5fcc2568076f9d6facb39bc0bec58a7ad6e0ee57
ch-tseng <ch.tseng@sunplusit.com>
2021-06-22 10:12:55 +0800

    'update'

--- voc_dataset/openimages_to_voc.py
+++ voc_dataset/openimages_to_voc.py
@@ -5,24 +5,32 @@ import pathlib
 from PIL import Image
 import cv2
 import sys
+from tqdm import tqdm 
 
-class_mapping = 'class-descriptions-boxable.csv'
+img_count_dataset = 50000  #split openimagesv6 to smaller dataset.
+
+class_mapping = r'K:\OpenImage_v6\Annotations\class-descriptions-boxable.csv'
 #want_class_list = { "Car", "Motorcycle", "Truck", "Bicycle", "Bus", "Van" }
 
-want_class_list = [ "Snake", "Crocodile", "Frog", "Turtle", "Tortoise", "Sea turtle", "Invertebrate", "Lizard", "Reptile", "Otter", "Jellyfish"]
-annotations_path = "/media/chtseng/AIHD01/OpenImage_v6/Annotations/boxes/oidv6-train-annotations-bbox.csv"
-images_path = "/media/chtseng/AIHD01/OpenImage_v6/dataset/train/"
+want_class_list = []
+#want_class_list = [ "Snake", "Crocodile", "Frog", "Turtle", "Tortoise", "Sea turtle", "Invertebrate", "Lizard", "Reptile", "Otter", "Jellyfish"]
+annotations_path = r"K:\OpenImage_v6\Annotations\boxes\oidv6-train-annotations-bbox.csv"
+images_path = r"K:\OpenImage_v6\dataset\train"
 
 xml_file = "xml_file.txt"
 object_xml_file = "xml_object.txt"
 
 #output
-datasetPath = "/media/chtseng/modelSALE/inSale/OpenImages_Himant_reptiles/"
+base_datasetPath = "E:/OpenImages_VOC/"
 imgPath = "images/"
 labelPath = "labels/"
 imgType = "jpg"  # jpg, png
 
 #----------------------------------------------------------------------------------------------------
+class_mapping = class_mapping.replace('\\', '/')
+annotations_path = annotations_path.replace('\\', '/')
+images_path = images_path.replace('\\', '/')
+datasetPath = base_datasetPath
 
 class_maps = {}
 revers_cmap = {}
@@ -36,12 +44,13 @@ with open(class_mapping, 'r', encoding='UTF-8') as cfile:
         revers_cmap.update( { cmap[0]:cmap[1] })
 
 no_class_list = ''
-for i, want_class in enumerate(want_class_list):
-    if want_class not in class_maps:
-        if i>0: no_class_list += ','
-        no_class_list += want_class
-    else:
-        target_class.append(class_maps[want_class])
+if want_class_list != []:
+    for i, want_class in enumerate(want_class_list):
+        if want_class not in class_maps:
+            if i>0: no_class_list += ','
+            no_class_list += want_class
+        else:
+            target_class.append(class_maps[want_class])
 
 if no_class_list != '':
     print('No these class for OpenImage V6:', no_class_list)
@@ -102,21 +111,23 @@ def makeLabelFile(filename, bboxes, imgfile):
 
 file_list = {}
 if __name__ == "__main__":
-    check_env()
-
+    #check_env()
+    print('annotations_path', annotations_path)
     f = open(annotations_path)
     lines = f.readlines()
     total_lines = len(lines)
-    for lineID, line in enumerate(lines):
+    lineID = 0
+    for line in tqdm(lines):
         data_annotations = line.split(',')
-        if(len(data_annotations)==21):
+        if(len(data_annotations)==21 and lineID>0):
+            #print('data_annotations', data_annotations)
             class_id = data_annotations[2]
-            if(class_id in target_class):
+            if(class_id in target_class or target_class==[]):
                 file_name = data_annotations[0]
                 type_anno = data_annotations[2]
 
                 minx,maxx,miny,maxy = data_annotations[4], data_annotations[5], data_annotations[6], data_annotations[7]
-                print('minx,maxx,miny,maxy ', minx,maxx,miny,maxy )
+                #print('minx,maxx,miny,maxy ', minx,maxx,miny,maxy )
                 if(file_name in file_list):
                     #print(file_list)
                     last_data = file_list[file_name]
@@ -130,9 +141,13 @@ if __name__ == "__main__":
                 else:
                     file_list.update({ file_name:[(revers_cmap[type_anno], [minx,maxx,miny,maxy])]})
 
+        lineID += 1
 
+    id = 0
+    count_now = 0
+    ds_id = 0
     total_lines = len(file_list)
-    for id, img_path in enumerate(pathlib.Path(images_path).iterdir()):
+    for img_path in tqdm(pathlib.Path(images_path).iterdir()):
         if img_path.is_file():
             filename = os.path.basename(img_path)
             base_name, file_extension = os.path.splitext(filename)
@@ -149,8 +164,23 @@ if __name__ == "__main__":
 
                     minx, maxx, miny, maxy = float(data[1][0])*width, float(data[1][1])*width, float(data[1][2])*height, float(data[1][3])*height
                     xx,yy,ww,hh = int(minx), int(miny), int(maxx-minx), int(maxy-miny)
-                    print("[{}/{}] {}:{} ---> {},{}".format(id, total_lines, filename, data[1], classname, [xx,yy,ww,hh]))
+                    #print("[{}/{}] {}:{} ---> {},{}".format(id, total_lines, filename, data[1], classname, [xx,yy,ww,hh]))
 
                     list_bboxes.append((classname,[xx,yy,ww,hh]))
 
+                folder_path_ds = "openimage_v6_voc_{}".format(ds_id)
+                datasetPath = os.path.join(base_datasetPath, folder_path_ds)
+                check_env()
+                '''
+                if not os.path.exists(datasetPath):
+                    os.makedirs( os.path.join(datasetPath, 'images'))
+                    os.makedirs( os.path.join(datasetPath, 'labels'))
+                '''
                 makeLabelFile(base_name, list_bboxes, img_file)
+                count_now += 1
+
+                if count_now>img_count_dataset:
+                    count_now = 0
+                    ds_id += 1
+
+        id += 1
